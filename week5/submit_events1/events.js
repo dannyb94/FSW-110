482f2b12a821fd6c0d662c8d1ec271d904c7e662
dannyb94 <danielle.ussery@bryanuniversity.edu>
2021-03-20 21:05:42 -0700

    week5

--- week5/submit_events1/events.js
+++ week5/submit_events1/events.js
@@ -3,8 +3,8 @@ It is important to note that any button placed inside of <form> tags will automa
 Your task for this assignment is to create a form on an html file that has at least 3 inputs and a button.
 
 
-• When the button is clicked, the inputs should be displayed in an “alert” window using the alert() method.  
-• After the alert is closed, the inputs should clear themselves out.
+X • When the button is clicked, the inputs should be displayed in an “alert” window using the alert() method.  
+X • After the alert is closed, the inputs should clear themselves out.
 */
 
 var pwd1 = document.getElementById("pswd1"),
@@ -21,7 +21,43 @@ function verify(){
 pwd1.addEventListener('change', verify);
 pwd2.addEventListener('keyup', verify);
 
+var btnClick = document.getElementById("sub");
+btnClick.addEventListener('click', (event) => {
+    var fn = document.getElementById("firstName").value;//firstn
+    var ln = document.getElementById("lastName").value;//lastn
+    var eml = document.getElementById("email").value;//email
+    var dob = document.getElementById("dob").value;//dob
+    var usrn = document.getElementById("username").value;//usr
+    var pw1 = document.getElementById("pswd1").value;//pw1
+    var pw2 = document.getElementById("pswd2").value;//pw2
+    alert(`First Name: ${fn} \nLast Name: ${ln} \nEmail: ${eml} \nDate of Birth: ${dob} \nUsername: ${usrn}`);
+    if (fn.length > 1){
+        document.getElementById('firstName').value = "";
+    }
+    if (ln.length > 1){
+        document.getElementById('lastName').value = "";
+    }
+    if (eml.length > 1){
+        document.getElementById('email').value = "";
+    }
+    if (dob.length > 1){
+        document.getElementById('dob').value = "";
+    }
+    if (usrn.length > 1){
+        document.getElementById('username').value = "";
+    }
+    if (pw1.length > 1){
+        document.getElementById('pswd1').value = "";
+    }
+    if (pw2.length > 1){
+        document.getElementById('pswd2').value = "";
+    }
+});
 
+
+
+
+/* 
 var btnClick = document.getElementById("sub");
 
 btnClick.addEventListener('click', (event) => {
@@ -35,3 +71,4 @@ function alertTime(a){
         //
     }
 }
+*/
